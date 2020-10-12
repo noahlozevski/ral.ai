91810ff4883dcf1c1986a46e3ccec16f631710b0
noah lozevski <55212226+noahlozevski@users.noreply.github.com>
2020-10-12 01:36:17 -0400

    Created initial vue proj

---
+++ ral.ai/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
+    "prettier/vue",
+
+    // "plugin:@prettier/pug",
+  ],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  plugins: ["vue", "prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    semi: [2, "never"],
+
+    "vue/max-attributes-per-line": "off",
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1,
+        maxEOF: 1,
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+        printWidth: 80,
+        arrayExpand: true,
+        embeddedLanguageFormatting: "auto",
+      },
+    ],
+    "lines-between-class-members": ["error", "always"],
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"],
+      },
+    ],
+  },
+}

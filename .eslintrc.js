ee39f3e12b1d7adb966a13f0abbb5cd7ba1a8b53
noah lozevski <55212226+noahlozevski@users.noreply.github.com>
2020-10-12 01:41:40 -0400

    updated folder structure and upgraded to vue 3.0 beta

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/recommended",
+    // consider switching to `plugin:vue/vue3-strongly-recommended` or `plugin:vue/vue3-recommended` for stricter rules.
+    "plugin:vue/vue3-recommended",
     "plugin:prettier/recommended",
     "prettier/vue",
 
